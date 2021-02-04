e88478154c7d2b5195c653dbff411e56f81d2892
Krzwk <44804099+Krzwk@users.noreply.github.com>
2021-02-04 13:39:09 +0100

    added Dart script, small changes to Boss

--- GroupPlatformer/Assets/Scripts/Boss.cs
+++ GroupPlatformer/Assets/Scripts/Boss.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    [SerializeField]
+    private int health;
+    public bool invincible;
+    public GameObject explosionPrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,24 @@ public class Boss : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnHit(int hpLoss){
+        health -= hpLoss;
+        if (health <= 0){
+            OnDeath();
+        }
+        invincible = true;
+        StartCoroutine(Hit());
+    }
+
+    private void OnDeath(){
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    IEnumerator Hit(){
+        yield return new WaitForSeconds(3);
+        invincible = false;
     }
 }
---
+++ GroupPlatformer/Assets/Scripts/Dart.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    [SerializeField]
+    private float dartSpeed = 30;
+
+    public GameObject explosionPrefab;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float amtToMove = dartSpeed * Time.deltaTime;
+        transform.Translate(Vector3.up * amtToMove);
+        if(transform.position.y > 6.4f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+            enemy.OnDeath();
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Boss"))
+        {
+            Boss boss = other.gameObject.GetComponent<Boss>();
+            if(!boss.invincible){
+                boss.OnHit(10);
+                }
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
